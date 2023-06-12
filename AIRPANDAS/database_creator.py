3fe3c77d884287d2f089d5cb3f2e877df6cdf610
Marugannwg <115759986+Marugannwg@users.noreply.github.com>
2023-06-12 01:36:12 -0700

    Update database_creator.py

--- AIRPANDAS/database_creator.py
+++ AIRPANDAS/database_creator.py
@@ -1,9 +1,20 @@
 import data_cleaning
 from sqlalchemy import create_engine
 
+'''
+This script is a sample of how to creates a SQL database from the CSV files we had from the AQS API and the Census API.
+
+For to replicate, first obtain the CSV files from the AQS API and the Census API,
+then change the file paths and names in the main() function.
+
+For AQS data, we only handle the sampleData, not the dailyData. (i.e. any AQS dataframe with hourly data)
+
+You can also change the name of the output database
+
+'''
+
 
 def main():
-    ## Create a SQL engine -- the name of the database is 
 
     output_database_name = 'airpandas_1.sqlite' ## YOU CAN CHANGE THIS NAME
     engine = create_engine(f'sqlite:///{output_database_name}')
@@ -13,8 +24,8 @@ def main():
         "Race" : r"database\census_data_RACE_2009-2021.csv",
         "Income" : r"database\census_data_INCOME_AND_BENEFITS_2009-2021.csv",
         "Education" : r"database\census_data_EDUCATIONAL_ATTAINMENT_2009-2021.csv",
-        "Employment" : r"database\census_data_EMPLOYMENT_STATUS _2009-2021.csv",
-        "Heating" : r"database\census_data_HOUSE_HEATING_FUEL _2009-2021.csv",
+        "Employment" : r"database\census_data_EMPLOYMENT_STATUS_2009-2021.csv",
+        "Heating" : r"database\census_data_HOUSE_HEATING_FUEL_2009-2021.csv",
         "Occupation" : r"database\census_data_OCCUPATION_2009-2021.csv",
         "Commute" : r"database\census_data_COMMUTING_TO_WORK_2009-2021.csv",
         "Industry" : r"database\census_data_INDUSTRY_2009-2021.csv",
@@ -27,8 +38,6 @@ def main():
 
     file_names = [r'database\2009_2014.6_CA_PM2.5_samples.csv', r'database\2014.6_2021_CA_PM2.5_samples.csv']
     data_cleaning.process_csv_in_chunks(file_names, 10000, engine, 'PM25')
-
-
 
 
 
